3d03901f6e3128f4139c864d616e3bc4dcfa6c01
Sakib Ahamed <35700378+zsxkib@users.noreply.github.com>
2024-09-09 23:03:13 +0200

    Fix/aspect ratios (#30)
    * Fix: aspect ratios now work correctly in predict.py

--- predict.py
+++ predict.py
@@ -344,6 +344,8 @@ class Predictor(BasePredictor):
         else:  # is_txt2img_mode
             print("[!] txt2img mode")
             pipe = self.pipes[model]
+            flux_kwargs["width"] = width
+            flux_kwargs["height"] = height
 
         if replicate_weights:
             flux_kwargs["joint_attention_kwargs"] = {"scale": lora_scale}
